0bf7dc38f55f071849bb00940db9a23031e03cf2
rakinu21 <rakinkinra@gmail.com>
2025-01-26 16:12:18 +0800

    complete user login register and logout and have generate token

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,6 @@
 import { User } from "../models/userShema.js";
 import bcryptjs from 'bcryptjs';
-
+import { generateToken } from "../utils/generateToken.js";
 
 export const register = async (req, res) => {
     
@@ -24,6 +24,59 @@ export const register = async (req, res) => {
             user: user
         })
 
+    } catch (error) {
+        res.status(500).json({message: `ERROR: ${error.message}`})
+    }
+}
+
+
+
+
+export const login = async (req, res) => {
+    
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) return res.status(400).json({ message: 'all input is required' });
+
+        //check if user is not exist;
+        const isUserValid = await User.findOne({ email });
+        if (!isUserValid) return res.status(401).json({ message: 'user not found' });
+
+        //check the password hashing and compare the user password;
+        const isPassword = bcryptjs.compareSync(password, isUserValid.password);
+        if (!isPassword) return res.status(401).json({ message: 'invalid credentials' });
+
+        const{password: hashPassword, ...rest} = isUserValid._doc
+
+        const token = generateToken(isUserValid._id);
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 1 * 24 * 60 * 60 * 1000
+        });
+
+        res.status(200).json({
+            message: 'login successfully',
+            success: true,
+            user: rest,
+            token: token
+         },
+        )
+
+    } catch (error) {
+         res.status(500).json({message: `ERROR: ${error.message}`})
+    }
+}
+
+export const logout = async (req, res) => {
+    
+    try {
+        res.cookie("token", '', {
+            httpOnly: true,
+            expires: new Date(0)
+        })
+        res.status(200).json({message:'logout successfully'})
     } catch (error) {
         res.status(500).json({message: `ERROR: ${error.message}`})
     }
--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { register } from '../controllers/userController.js';
+import { login, logout, register } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', register)
-
+userRouter.post('/login', login)
+userRouter.post('/logout', logout)
 export default userRouter
